4ce8646dcb1cd025b990a0d67838ae1d7d1095d9
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2021-08-12 18:24:19 +0300

    feat: (ch11 separation of responsibilities) (authentication server) add logging

--- s11-separation-of-responsibilities/authentication-server/src/main/java/com/example/demo/controller/AuthController.java
+++ s11-separation-of-responsibilities/authentication-server/src/main/java/com/example/demo/controller/AuthController.java
@@ -4,12 +4,14 @@ import com.example.demo.dto.OtpDto;
 import com.example.demo.dto.UserDto;
 import com.example.demo.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
@@ -18,16 +20,19 @@ public class AuthController {
 
     @PostMapping("/user/add")
     void addUser(@RequestBody UserDto userDto) {
+        log.debug("addUser: {}", userDto);
         userService.addUser(userDto);
     }
 
     @PostMapping("/user/auth")
     void auth(@RequestBody UserDto userDto) {
+        log.debug("auth: {}", userDto);
         userService.auth(userDto);
     }
 
     @PostMapping("/otp/check")
     void auth(@RequestBody OtpDto otpDto, HttpServletResponse response) {
+        log.debug("check: {}", otpDto);
         if (userService.check(otpDto)) {
             response.setStatus(HttpServletResponse.SC_OK);
         } else {
